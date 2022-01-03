5128ca0b7868a680f866a7320ee7e15ff7a4afc2
SeoyeonBae <83990943+cindy0817-web@users.noreply.github.com>
2022-01-03 11:48:05 +0900

    출력 부분 추가

--- After-Class/src/test/DynamicArrTest.java
+++ After-Class/src/test/DynamicArrTest.java
@@ -11,7 +11,13 @@ public class DynamicArrTest {
 			nums[i] = new int[i+1];
 			for (int j = 0; j < nums[i].length; j++) 
 				nums[i][j] = count++ * 10;
-			
+		}
+		
+		for (int i = 0 ; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				System.out.print(nums[i][j] +"  ");
+			}
+			System.out.println();
 		}
 	}
 
