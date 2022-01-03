1b1678f4d284b530f35322568aea0bec08d96b29
SeoyeonBae <83990943+cindy0817-web@users.noreply.github.com>
2022-01-03 11:40:55 +0900

    Add files via upload

---
+++ After-Class/src/test/Array2Test.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Array2Test {
+
+	public static void main(String[] args) {
+		String[][] favs = {{"닭", "강아지", "토끼", "사자"},{"사과", "바나나", "귤", "키위"}};
+		
+		for (int i = 0; i < favs.length; i++) {
+			for (int j = 0; j < favs[i].length; j++){
+				System.out.print(favs[i][j] + " ");
+			}
+			System.out.println();
+		}
+		int[][] favs2 = new int [3][2];
+		
+		int count = 1;
+		
+		for (int i = 0; i < favs2.length; i++) 
+			for(int j = 0; j < favs2[i].length; j++) {
+				favs2[i][j]= count++ * 10;
+				/*favs2[i][j] = count;
+				count += 10;*/
+			}
+		
+		for (int i = 0; i < favs2.length; i++) {	//6
+			for (int j = 0; j < favs2[i].length; j++) 	//3
+				System.out.print(favs2[i][j] + " ");
+			System.out.println();
+		}
+	}
+}
---
+++ After-Class/src/test/DynamicArrTest.java
@@ -0,0 +1,18 @@
+package test;
+
+public class DynamicArrTest {
+
+	public static void main(String[] args) {
+//		int [][] nums = {{10}, {20, 30}, {40,50, 60}};
+		int [] [] nums = new int[3][];
+		
+		int count = 1;
+		for (int i = 0; i<nums.length; i++) {
+			nums[i] = new int[i+1];
+			for (int j = 0; j < nums[i].length; j++) 
+				nums[i][j] = count++ * 10;
+			
+		}
+	}
+
+}
