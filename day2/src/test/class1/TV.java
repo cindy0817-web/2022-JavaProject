333742081ff60a6bf10c7417e4dd42035434f07c
SeoyeonBae <83990943+cindy0817-web@users.noreply.github.com>
2022-01-04 11:49:39 +0900

    tv 클래스 모델링 후 필드와 메소드 추가

---
+++ day2/src/test/class1/TV.java
@@ -0,0 +1,29 @@
+package test.class1;
+
+public class TV{
+	
+	private String color;
+	
+	String producer;
+	int size, channel, volume;
+	
+	void powerOn(){
+		
+	}
+	void powerOff(){
+		
+	}
+	void UpChannel(){
+		
+	}
+	void DownChannel() {
+		
+	}
+	void upVolume() {
+		
+	}
+	void DownVolume() {
+		
+	}
+}
+
