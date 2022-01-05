f1121c1d422c20e3b886480bea611aa914c3daef
SeoyeonBae <83990943+cindy0817-web@users.noreply.github.com>
2022-01-05 11:05:34 +0900

    두 수의 합계 계산하는 클래스

--- day3/src/class2/RunClassAdd.java
+++ day3/src/class2/RunClassAdd.java
@@ -10,15 +10,17 @@ public class RunClassAdd {
 		a2.setN2(20);
 		a2.addNum();
 		
-		System.out.printf("%d + %d = %d\n", a2.getN1(), a2.getN2(), a2.getResult());
-		//System.out.println("a2.result = " + a2.getResult());
 		
-		a2.setN1(150);
+		System.out.printf("%d + %d = %d\n", a2.getN1(), a2.getN2(), a2.getResult());
+//		System.out.println("a2.result = " + a2.getResult());
+		
+		 a2.setN1(150); 
 		a2.setN2(200);
 		a2.addNum();
 		
 		System.out.printf("%d + %d = %d\n", a2.getN1(), a2.getN2(), a2.getResult());
-		//System.out.println("a2.result = " + a2.getResult());
+//		System.out.println("a2.result = " + a2.getResult());
+		
 	}
 
 }
