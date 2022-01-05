78e52f6ac92dd76bf4c6a579b5e9d3f935d86ac5
SeoyeonBae <83990943+cindy0817-web@users.noreply.github.com>
2022-01-05 11:48:31 +0900

    오버로드

---
+++ day3/src/overload1/OverloadTest1.java
@@ -0,0 +1,24 @@
+package test.overload1;
+
+public class OverloadTest1 {
+	private int result1, result2;
+
+	public int getResult1() {
+		return result1;
+	}
+	
+	public int getResult2() {
+		return result2;
+	}
+	
+	public void calc(int n1, int n2) {
+		result1 = n1 * n2;
+	}
+	
+	public void calc(int n1, int n2, int n3) {
+		result2 = n1 + n2 + n3;
+	}
+	public void calc(double n1, double n2) {
+		result2 = n1 * n2;
+	}
+}
