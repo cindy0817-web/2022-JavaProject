4576038246855113fe4a54e56f7949648d06ca50
Seoyeon Bae <83990943+seoyeon-double-7@users.noreply.github.com>
2022-01-11 09:49:16 +0900

    Add files via upload

---
+++ day7/src/test/constructorcall2/ClsA.java
@@ -0,0 +1,12 @@
+package test.constructorcall2;
+
+public class ClsA {
+	
+	public ClsA(){
+		System.out.println("ClsA의 기본생성자입니다.");
+	}
+	
+	public ClsA(int n) {
+		System.out.println("ClassA(int n)의 생성자 호출");
+	}
+}
---
+++ day7/src/test/constructorcall2/RunClsC.java
@@ -0,0 +1,12 @@
+package test.constructorcall2;
+
+public class RunClsC {
+
+	public static void main(String[] args) {
+		ClsC c1 = new ClsC();
+		
+		ClsC c2 = new ClsC(5);
+
+	}
+
+}
