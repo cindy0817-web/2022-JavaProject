e7e3f155227400aa5bf7a56534c59cd729ae27f5
Seoyeon Bae <83990943+seoyeon-double-7@users.noreply.github.com>
2022-01-11 10:37:26 +0900

    Add files via upload

---
+++ day7/src/test/overrriding1/Bomb.java
@@ -0,0 +1,10 @@
+package test.overrriding1;
+
+public class Bomb extends Weapon {
+
+	@Override
+	public void fire() {
+		System.out.println("½´¿õ~Æã");
+	}
+
+}
---
+++ day7/src/test/overrriding1/Weapon.java
@@ -0,0 +1,8 @@
+package test.overrriding1;
+
+public class Weapon {
+	
+	public void fire() {
+		System.out.println("≈¡≈¡≈¡");
+	}
+}
